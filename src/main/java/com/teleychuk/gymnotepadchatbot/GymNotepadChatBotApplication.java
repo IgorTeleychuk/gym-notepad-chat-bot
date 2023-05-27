a34d08fe4854d0829dda69764842c005cd9cbfcb
IgorTeleychuk <teleychuk.i@yandex.r>
2023-05-27 15:41:25 +0700

    Initial commit

---
+++ src/main/java/com/teleychuk/gymnotepadchatbot/GymNotepadChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.teleychuk.gymnotepadchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymNotepadChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymNotepadChatBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teleychuk/gymnotepadchatbot/GymNotepadChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teleychuk.gymnotepadchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymNotepadChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
